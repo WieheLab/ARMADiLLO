6af3d17152b18ad729f529a78b679c0dc8f0f740
jsm56 <jsm56@duke.edu>
2022-05-04 14:01:39 -0400

    fixed tokenize function in the utilities.cpp

--- utilities.cpp
+++ utilities.cpp
@@ -50,20 +50,20 @@ void translate_dna_to_aa(string &dna, string &aa, int reading_frame, map<string,
 
 void tokenize(const string& str, std::vector<string>& tokens, const string& delimiters = " ")
 {
-    // Skip delimiters at beginning.
+    // Skip delimiters at beginning.                                                                                                                        
     string::size_type lastPos = str.find_first_not_of(delimiters, 0);
-    // Find first "non-delimiter".
-    string::size_type pos     = str.find_first_of(delimiters, lastPos);
-
-    while (string::npos != pos || string::npos != lastPos)
-    {
-        // Found a token, add it to the vector.
-        tokens.push_back(str.substr(lastPos, pos - lastPos));
-        // Skip delimiters.  Note the "not_of"
-        lastPos = str.find_first_not_of(delimiters, pos);
-        // Find next "non-delimiter"
-        pos = str.find_first_of(delimiters, lastPos);
+    // Find first "non-delimiter".                                                                                                                          
+    //string::size_type pos     = str.find_first_of(delimiters, lastPos);                                                                                   
+    size_t pos=0;
+    string strtmp=str;
+    std::string token;
+    while ((pos = strtmp.find(delimiters)) != std::string::npos) {
+      token = strtmp.substr(0, pos);
+      tokens.push_back(token);
+      strtmp.erase(0, pos + delimiters.length() );
     }
+    tokens.push_back(strtmp);
+
 }
 
 
